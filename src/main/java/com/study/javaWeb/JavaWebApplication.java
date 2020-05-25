893fad45e739a0179998dfb6be9d60dcafce5221
drogba <drogba@wink.kr>
2020-05-25 20:27:31 +0900

    init

---
+++ src/main/java/com/study/javaWeb/JavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.study.javaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApplication.class, args);
+	}
+
+}
