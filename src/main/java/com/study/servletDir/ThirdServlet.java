2a77a3ac89bd8b6d279d4a92322e9ba35f83f478
drogba <drogba02@naver.com>
2020-05-26 19:25:26 +0900

    Servlet 실습#1

---
+++ src/main/java/com/study/servletDir/ThirdServlet.java
@@ -0,0 +1,29 @@
+package com.study.servletDir;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//어노테이션을 사용한 서블릿 매핑
+@WebServlet
+public class ThirdServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("ThirdServlet init 메서드 호출");
+    }
+
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException , IOException {
+        System.out.println("ThirdServlet doGet 메서드 호출");
+    }
+
+    protected void doPost(HttpServletRequest request , HttpServletResponse response) throws ServletException , IOException {
+        System.out.println("ThirdServlet destroy 메서드 호출");
+    }
+}
